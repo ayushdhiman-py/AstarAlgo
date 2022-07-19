26614687e266a870a80936af387c989870f6522f
ayushdhiman-py <ayushdhiman0107@gmail.com>
2022-07-19 23:43:33 +0530

    a star algo

---
+++ aStarLearn.py
@@ -0,0 +1,22 @@
+# from turtle import width
+# import pygame
+# import math
+# from queue import PriorityQueue
+
+# WIDTH = 900
+# WIN = pygame.display.set_mode((WIDTH,WIDTH))
+# pygame.display.set_caption("A* Path Finder")
+
+# ORANGE = (255, 165 ,0)
+# BLUE = (0, 255, 0)
+# BLACK = (0, 0, 0)
+# PURPLE = (128, 0, 128)
+# RED = (255, 0, 0)
+# GREEN = (0, 255, 0)
+# YELLOW = (255, 255, 0)
+# WHITE = (255, 255, 255)
+# GREY = (128, 128, 128)
+# TURQUOISE = (64, 224, 208)
+
+# class Spot:
+    
